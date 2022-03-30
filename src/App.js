511d48f9691ffe7e98ddb84e3041898ca687e712
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-29 22:10:59 -0500

    verificadores ruta publica privada

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   Route,
   BrowserRouter as Router,
   Switch,
-  Redirect,
+  Navigate,
 } from "react-router-dom";
 import Home from './pages/Home';
 import Chat from './pages/Chat';
@@ -11,10 +11,42 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 import { auth } from './services/firebase';
 
+function PrivateRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authenticated === true ? (
+          <Component {...props} />
+        ) : (
+          <Navigate
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function PublicRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authenticated === false ? (
+          <Component {...props} />
+        ) : (
+          <Navigate to="/chat" />
+        )
+      }
+    />
+  );
+}
+
 function App() {
   return (
     <div >
-     
+
     </div>
   );
 }
